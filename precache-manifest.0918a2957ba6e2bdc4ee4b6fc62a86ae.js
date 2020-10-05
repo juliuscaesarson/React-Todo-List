996f00e8f3f0bed1bedba2ea95b2c56be554caf8
Albert Son <54781741+juliuscaesarson@users.noreply.github.com>
2020-10-05 17:46:08 -0500

    Updates

---
+++ precache-manifest.0918a2957ba6e2bdc4ee4b6fc62a86ae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "36784c1543e32c0a967d",
+    "url": "/React-Todo-List/static/css/main.7dc6db61.chunk.css"
+  },
+  {
+    "revision": "36784c1543e32c0a967d",
+    "url": "/React-Todo-List/static/js/main.36784c15.chunk.js"
+  },
+  {
+    "revision": "90468e4dec592b52b52e",
+    "url": "/React-Todo-List/static/js/1.90468e4d.chunk.js"
+  },
+  {
+    "revision": "39f7598f9e4f9d06b55b",
+    "url": "/React-Todo-List/static/js/runtime~main.39f7598f.js"
+  },
+  {
+    "revision": "33b5de045ff95dc5968ef8c84188afea",
+    "url": "/React-Todo-List/index.html"
+  }
+];
